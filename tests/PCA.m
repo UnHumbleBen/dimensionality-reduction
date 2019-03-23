86c5cecad705db4f59d89860483c7e167894d402
Benjamin Lee <bnllee@ucdavis.edu>
2019-03-23 16:37:00 -0700

    added tests/PCA.m to test PCA implementation

---
+++ tests/PCA.m
@@ -0,0 +1,7 @@
+%% Initialization
+clear ; close all; clc
+
+%% Loads Example Dataset 1 and stores it as X
+load('../data/example-dataset1.mat');
+
+fprintf('\nRunning PCA on example dataset.\n\n');
