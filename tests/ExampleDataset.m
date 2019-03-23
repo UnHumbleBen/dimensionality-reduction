1d319b4ff6a50708fd8474048613dbeac3d82ffb
Benjamin Lee <bnllee@ucdavis.edu>
2019-03-23 16:05:28 -0700

    added code to plot Example Dataset 1 in tests/ExampleDataset

--- tests/ExampleDataset.m
+++ tests/ExampleDataset.m
@@ -3,5 +3,12 @@ clear ; close all; clc
 
 fprintf('Visualizing example dataset for PCA.\n\n');
 
-% Loads Example Dataset 1 and stores it as X
+%% Loads Example Dataset 1 and stores it as X
 load('../data/example-dataset1.mat');
+
+%% Visualize the example dataset
+% @param X(:, 1), the vector to plot against below
+% @param X(:, 2), the vector to plot against above
+% @param 'bo'   , format argument, where 'b' specifies
+%                 blue and 'o' specifies circles
+plot(X(:, 1), X(:, 2), 'bo');
