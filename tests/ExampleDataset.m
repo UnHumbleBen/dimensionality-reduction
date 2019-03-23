cb13ece702f56866eae3fa7a0be1f8f161e1f26f
Benjamin Lee <bnllee@ucdavis.edu>
2019-03-23 16:11:35 -0700

    added axis properties to the plot in tests/ExampleDataset.m

--- tests/ExampleDataset.m
+++ tests/ExampleDataset.m
@@ -12,3 +12,8 @@ load('../data/example-dataset1.mat');
 % @param 'bo'   , format argument, where 'b' specifies
 %                 blue and 'o' specifies circles
 plot(X(:, 1), X(:, 2), 'bo');
+
+% changing axis properties
+% @param [0.5, 6.5, 2, 8] specifies the axis limits
+% @param "square" specifies a square aspect ratio
+axis([0.5, 6.5, 2, 8], "square");
