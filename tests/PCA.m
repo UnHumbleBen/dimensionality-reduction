5abffa2b68761754bdb35d0797013741975d5296
Benjamin Lee <bnllee@ucdavis.edu>
2019-03-23 17:05:31 -0700

    added code to normalize X in tests/PCA.m

--- tests/PCA.m
+++ tests/PCA.m
@@ -1,7 +1,12 @@
 %% Initialization
 clear ; close all; clc
 
+addpath("../src");
+
 %% Loads Example Dataset 1 and stores it as X
 load('../data/example-dataset1.mat');
 
 fprintf('\nRunning PCA on example dataset.\n\n');
+
+%% normalize X
+[X_norm, mu, sigma] = featureNormalize(X);
