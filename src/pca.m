ca40f4a48b39f2c341cce69b13d3fe085add7da9
Benjamin Lee <bnllee@ucdavis.edu>
2019-03-23 18:23:25 -0700

    implemented pca() in src/pca.m

--- src/pca.m
+++ src/pca.m
@@ -3,5 +3,12 @@ function [U, S] = pca(X)
 %   [U, S, X] = pca(X) computes eigenvectors of the covariance
 %   Returns the eigenvectors U, the eigenvalues (on diagonal) in S
 
+% Stores m, number of training examples
+% and n, the number of features (dimensions)
+[m, n] = size(X);
+
+% Compute covariance matrix
+Sigma = (1/m)* X' * X;
+[U, S, ~] = svd(Sigma);
 
 end
