5b65d9bb326a919d9e702d63ea9588e24687e8d0
Benjamin Lee <bnllee@ucdavis.edu>
2019-03-24 16:55:58 -0700

    documented function in src/projectData.m

---
+++ src/projectData.m
@@ -0,0 +1,10 @@
+function Z = projectData(X, U, K)
+%PROJECTDATA Computes the reduced data representation when projecting
+%only on the top k eigenvectors
+%   Z = projectData(X, U, K) computes the projection of
+%   the normalized inputs X into the reduced dimensionality
+%   space spanned by the first K columns of U. It returns the
+%   projected examples in Z.
+%
+
+end
