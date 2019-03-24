4a07a38f20d5bacd95d38de1937c8e7c012d03df
Benjamin Lee <bnllee@ucdavis.edu>
2019-03-23 18:11:26 -0700

    document pca in src/pca.m

---
+++ src/pca.m
@@ -0,0 +1,7 @@
+function [U, S] = pca(X)
+%PCA Run principal component analysis on the dataset X
+%   [U, S, X] = pca(X) computes eigenvectors of the covariance
+%   Returns the eigenvectors U, the eigenvalues (on diagonal) in S
+
+
+end
