1398c9e64ba95ef65d83aac10dbe8fbf73fe6b19
Benjamin Lee <bnllee@ucdavis.edu>
2019-03-24 20:27:31 -0700

    documented displayData() in src/displayData.m

---
+++ src/displayData.m
@@ -0,0 +1,7 @@
+function [h, display_array] = displayData(X, example_width)
+%DISPLAYDATA Display 2D data in a nice grid
+%   [h, display_array] = DISPLAYDATA(X, example_width) displays 2D data
+%   stored in X in anice grid. It returns the figure handle h and the
+%   displayed array if requested
+
+end
