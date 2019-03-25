98117fbdd7df917bab359384e6021264f8a6c066
Benjamin Lee <bnllee@ucdavis.edu>
2019-03-24 17:14:50 -0700

    documented function src/recoverData.m

---
+++ src/recoverData.m
@@ -0,0 +1,8 @@
+function X_rec = recoverData(Z, U, K)
+%RECOVERDATA Recovers an approximation of the original
+%data when using the projected data
+%   X_rec = RECOVERDATA(Z, U, K) recovers an approximation of the
+%   original data that has been reduced to K dimensions. It returns
+%   the approixmate reconstruction in X_rec.
+
+end
