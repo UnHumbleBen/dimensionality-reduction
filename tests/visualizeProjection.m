429cdd781fad927a12f703bcb6773657366d9ac0
Benjamin Lee <bnllee@ucdavis.edu>
2019-03-24 18:08:06 -0700

    added test for recoverData() in tests/visualizeProjection.m

--- tests/visualizeProjection.m
+++ tests/visualizeProjection.m
@@ -11,3 +11,7 @@ K = 1;
 Z = projectData(X_norm, U, K);
 fprintf('Projection of the first example: %f\n', Z(1));
 fprintf('\n(this value should be about 1.481274)\n\n');
+
+X_rec = recoverData(Z, U, K);
+fprintf("Approximation of the first example: %f %f\n", X_rec(1, 1), X_rec(1, 2));
+fprintf("\n(this value should be agout -1.047419 -1.047419)\n\n");
