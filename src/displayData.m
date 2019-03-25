1d8a034a607bea957cf52ed68d1ac7bc62d969f1
Benjamin Lee <bnllee@ucdavis.edu>
2019-03-24 21:07:07 -0700

    adding images to display array in src/displayData.m

--- src/displayData.m
+++ src/displayData.m
@@ -27,6 +27,25 @@ display_array = - ones(pad + display_rows * (example_height + pad), ...
                        pad + display_cols * (example_width  + pad));
 
 % Copy each example into a patch on the display array
+curr_ex = 1;
+for j = 1:display_rows
+    for i = 1:display_cols
+        if curr_ex > m,
+            break;
+        end
+
+        % Get the max value of the patch
+        max_val = max(abs(X(curr_ex, :)));
+        curr_rows = pad + (j - 1) * (example_height + pad) + (1:example_height);
+        curr_cols = pad + (i - 1) * (example_width  + pad) + (1:example_width );
+        curr_reshaped = reshape(X(curr_ex, :), example_height, example_width);
+        display_array(curr_rows, curr_cols) = curr_reshaped / max_val;
+        curr_ex = curr_ex + 1;
+    end
+    if curr_ex > m,
+        break
+    end
+end
 
 % Gray Image
 % colormap (map) sets the current colormap to map
@@ -36,5 +55,14 @@ colormap(gray);
 
 % Display Image
 h = imagesc(display_array, [-1 1]);
+
+% Do not show axis
+% "image" forces x distance to equal y distance
+% and fixes axes to the limits of the data
+% "off" turns of tic marks
+axis("image", "off");
+
+% Flushes figure window
+drawnow;
 
 end
