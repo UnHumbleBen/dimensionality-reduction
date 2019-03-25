f364b3fb3966abde8dd2dc1434ebc2d84968539e
Benjamin Lee <bnllee@ucdavis.edu>
2019-03-24 17:06:28 -0700

    added test for projectData in tests/visualizeProjection.m

--- tests/visualizeProjection.m
+++ tests/visualizeProjection.m
@@ -5,3 +5,9 @@ fprintf('\nDimension reduction on example dataset.\n\n');
 %   Plot the normalized dataset (returned from featureNormalize)
 plot(X_norm(:, 1), X_norm(:, 2), 'bo');
 axis([-4 3 -4 3]); axis square
+
+%   Project the data onto K = 1 dimension
+K = 1;
+Z = projectData(X_norm, U, K);
+fprintf('Projection of the first example: %f\n', Z(1));
+fprintf('\n(this value should be about 1.481274)\n\n');
